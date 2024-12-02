3f614957f99c5965afc83ca5238a176c9ca1c740
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-02 19:15:47 +1100

    Add day 2, part 2

--- days/2/answer.ts
+++ days/2/answer.ts
@@ -1,36 +1,46 @@
 import { AnswerFunction } from "../../answer.ts";
 
-export const answer: AnswerFunction = ([input]) => {
-  const reports = input
+function parseReports(input: string): number[][] {
+  return input
     .split("\n")
     .filter(Boolean)
     .map((report) =>
       report.split(" ").map((level) => Number.parseInt(level, 10))
+    )
+    .map((report) =>
+      report.at(0) > report.at(-1) ? report.toReversed() : report
     );
+}
 
-  const safeReports = reports
-    .map((report) => {
-      if (report.at(0) > report.at(-1)) {
-        report = report.toReversed();
-      }
+function isSafeReport(report: number[]): boolean {
+  return report.every((curr, index) => {
+    if (index === 0) {
+      return true;
+    }
+    const prev = report[index - 1];
+    return [1, 2, 3].includes(curr - prev);
+  });
+}
 
-      try {
-        report.reduce((prev, curr) => {
-          if (prev === null) {
-            return curr;
-          }
+function isDampenedReport(report: number[]): boolean {
+  if (isSafeReport(report)) {
+    return true;
+  }
 
-          if (![1, 2, 3].includes(curr - prev)) {
-            throw new Error(`unsafe! ${curr} ${prev}`);
-          }
-          return curr;
-        }, null);
-        return true;
-      } catch {
-        return false;
-      }
-    })
-    .filter(Boolean).length;
+  for (let index = 0; index < report.length; index++) {
+    if (isSafeReport(report.toSpliced(index, 1))) {
+      return true;
+    }
+  }
 
-  return [safeReports.toString(), ""];
+  return false;
+}
+
+export const answer: AnswerFunction = ([input]) => {
+  const reports = parseReports(input);
+
+  const safeReports = reports.filter(isSafeReport).length;
+  const dampenedReports = reports.filter(isDampenedReport).length;
+
+  return [safeReports.toString(), dampenedReports.toString()];
 };
