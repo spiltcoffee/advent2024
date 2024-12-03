411128b2c92b3b5a4bbb10d9faf82000531623c8
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-03 16:55:52 +1100

    Day 3 answers

--- days/3/answer.ts
+++ days/3/answer.ts
@@ -1,5 +1,22 @@
 import { AnswerFunction } from "../../answer.ts";
 
-export const answer: AnswerFunction = () => {
-  return ["", ""];
+const MUL_REGEX = /mul\((\d{1,3}),(\d{1,3})\)/g;
+const DONT_REGEX = /don't\(\).*?(do\(\)|$)/gs;
+
+function getMultiplierString(input: string): string {
+  return Array.from(
+    input.matchAll(MUL_REGEX).map(([_, left, right]) => `${left} * ${right}`)
+  ).join(" + ");
+}
+
+function stripDisabled(input: string): string {
+  return input.split(DONT_REGEX).join("");
+}
+
+export const answer: AnswerFunction = ([input]) => {
+  const basicResult = eval(getMultiplierString(input));
+
+  const enabledResult = eval(getMultiplierString(stripDisabled(input)));
+
+  return [basicResult.toString(), enabledResult.toString()];
 };
