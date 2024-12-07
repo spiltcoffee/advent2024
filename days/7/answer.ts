c97bfbfc81b58bde9de0ea9b8d5a74d7b68f8975
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-07 22:08:08 +1100

    Add day 7 part 2

--- days/7/answer.ts
+++ days/7/answer.ts
@@ -1,41 +1,58 @@
 import { AnswerFunction } from "../../answer.ts";
 
 class Calibration {
-  readonly target: number;
-  private readonly values: number[];
+  readonly target: bigint;
+  private readonly values: bigint[];
 
-  private constructor(target: number, values: number[]) {
+  private constructor(target: bigint, values: bigint[]) {
     this.target = target;
     this.values = values;
   }
 
   static fromLine(line: string) {
     const [targetStr, valuesStr] = line.split(":");
-    const target = Number.parseInt(targetStr, 10);
-    if (target > Number.MAX_SAFE_INTEGER) {
-      throw new Error(target + "is too big!");
-    }
+    const target = BigInt(targetStr);
     const values = valuesStr
       .trim()
       .split(" ")
-      .map((value) => Number.parseInt(value, 10));
+      .map((value) => BigInt(value));
     return new Calibration(target, values);
   }
 
-  private static checkValues(target: number, values: number[]): boolean {
+  private static checkValuesPart1(target: bigint, values: bigint[]): boolean {
     const [left, right, ...rest] = values;
     if (!right) {
       return left === target;
     } else {
       return (
-        Calibration.checkValues(target, [left + right, ...rest]) ||
-        Calibration.checkValues(target, [left * right, ...rest])
+        Calibration.checkValuesPart1(target, [left + right, ...rest]) ||
+        Calibration.checkValuesPart1(target, [left * right, ...rest])
       );
     }
   }
 
-  isValid(): boolean {
-    return Calibration.checkValues(this.target, this.values);
+  private static checkValuesPart2(target: bigint, values: bigint[]): boolean {
+    const [left, right, ...rest] = values;
+    if (!right) {
+      return left === target;
+    } else {
+      return (
+        Calibration.checkValuesPart2(target, [left + right, ...rest]) ||
+        Calibration.checkValuesPart2(target, [left * right, ...rest]) ||
+        Calibration.checkValuesPart2(target, [
+          BigInt(left.toString() + right.toString()),
+          ...rest
+        ])
+      );
+    }
+  }
+
+  isValidPart1(): boolean {
+    return Calibration.checkValuesPart1(this.target, this.values);
+  }
+
+  isValidPart2(): boolean {
+    return Calibration.checkValuesPart2(this.target, this.values);
   }
 }
 
@@ -45,9 +62,13 @@ export const answer: AnswerFunction = ([input]) => {
     .filter(Boolean)
     .map(Calibration.fromLine);
 
-  const validTotal = calibrations
-    .filter((calibration) => calibration.isValid())
-    .reduce((total, curr) => total + BigInt(curr.target), BigInt(0));
+  const part1Total = calibrations
+    .filter((calibration) => calibration.isValidPart1())
+    .reduce((total, curr) => total + curr.target, BigInt(0));
 
-  return [validTotal.toString(), ""];
+  const part2Total = calibrations
+    .filter((calibration) => calibration.isValidPart2())
+    .reduce((total, curr) => total + curr.target, BigInt(0));
+
+  return [part1Total.toString(), part2Total.toString()];
 };
