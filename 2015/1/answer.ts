32f10fc4a84e3736296ff05bac991f9a9029b977
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-15 18:02:30 +1100

    Add 2015 day 1 part 2

--- 2015/1/answer.ts
+++ 2015/1/answer.ts
@@ -1,8 +1,13 @@
 import { AnswerFunction } from "../../answer.ts";
 
 export const answer: AnswerFunction = ([input]) => {
-  const floor = input
-    .split("")
-    .reduce((floor, char) => floor + (char === "(" ? 1 : -1), 0);
-  return [floor.toString(), ""];
+  let basementPos: number = NaN;
+  const floor = input.split("").reduce((floor, char, index) => {
+    floor += char === "(" ? 1 : -1;
+    if (floor === -1 && !basementPos) {
+      basementPos = index + 1;
+    }
+    return floor;
+  }, 0);
+  return [floor.toString(), basementPos.toString()];
 };
