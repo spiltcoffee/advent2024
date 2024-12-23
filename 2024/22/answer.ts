0df54bc94795877a075db3cfee77403a70785d8e
spiltcoffee <spiltcoffee@users.noreply.github.com>
2024-12-23 12:54:29 +1100

    Add day 22 part 2

--- 2024/22/answer.ts
+++ 2024/22/answer.ts
@@ -10,11 +10,50 @@ function mixAndPrune(s: number): number {
 }
 
 export const answer: AnswerFunction = ([input]) => {
-  const secretsAfter2000 = input
+  const initialSecrets = input
     .split("\n")
-    .map((str) => Number.parseInt(str, 10))
-    .map((secret) => range(2000).reduce(mixAndPrune, secret));
+    .map((str) => Number.parseInt(str, 10));
 
-  const total = secretsAfter2000.reduce((total, secret) => total + secret, 0);
-  return [total.toString(), ""];
+  const secretsTotalAfter2000 = initialSecrets
+    .map((secret) => range(2000).reduce(mixAndPrune, secret))
+    .reduce((total, secret) => total + secret, 0);
+
+  const sequenceTally = initialSecrets.reduce<Record<string, number>>(
+    (tally, secret) => {
+      const found = new Set<string>();
+      let lastBananas = secret % 10;
+      let diffSequence = [];
+      range(2000).reduce((secret) => {
+        secret = mixAndPrune(secret);
+
+        const bananas = secret % 10;
+        const diff = bananas - lastBananas;
+        if (!Number.isNaN(diff)) {
+          diffSequence.push(diff);
+          if (diffSequence.length === 4) {
+            const seqId = diffSequence.join();
+            if (!found.has(seqId)) {
+              tally[seqId] ||= 0;
+              tally[seqId] += bananas;
+              found.add(seqId);
+            }
+            diffSequence = diffSequence.slice(1);
+          }
+        }
+
+        lastBananas = bananas;
+        return secret;
+      }, secret);
+
+      return tally;
+    },
+    {}
+  );
+
+  // console.log(sequenceTally);
+  const bestBananas = Object.values(sequenceTally)
+    .sort((a, b) => b - a)
+    .at(0);
+
+  return [secretsTotalAfter2000.toString(), bestBananas.toString()];
 };
